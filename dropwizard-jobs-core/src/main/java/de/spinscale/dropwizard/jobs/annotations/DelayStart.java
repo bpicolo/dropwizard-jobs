808816125ce506038c91f12f4e24f3f3789d8f5e
Benjamin Picolo <ben@tegus.co>
2018-03-16 10:52:28 -0400

    import fix

--- dropwizard-jobs-core/src/main/java/de/spinscale/dropwizard/jobs/annotations/DelayStart.java
+++ dropwizard-jobs-core/src/main/java/de/spinscale/dropwizard/jobs/annotations/DelayStart.java
@@ -1,6 +1,7 @@
 package de.spinscale.dropwizard.jobs.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
